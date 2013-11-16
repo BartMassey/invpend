11705f53e815c320e65406397032ce88cf508c95
Bart Massey <bart@cs.pdx.edu>
2013-11-16 10:12:45 -0800

    got basic checked background graphics working

--- graphics.c
+++ graphics.c
@@ -9,6 +9,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <cairo/cairo.h>
 #include <cairo/cairo-xcb.h>
@@ -31,6 +32,30 @@ int window_active = 0;
  * I followed.
  */
 
+void check_request(char *desc, xcb_void_cookie_t r) {
+    xcb_generic_error_t *e = xcb_request_check(c, r);
+    if (!e)
+        return;
+    fprintf(stderr, "xcb request check failed on %s:\n", desc);
+    fprintf(stderr, "  error: %u\n", e->error_code);
+    fprintf(stderr, "  opcode: %u/%u\n", e->major_code, e->minor_code);
+    fprintf(stderr, "  sequence: %u\n", e->full_sequence);
+    exit(1);
+}
+
+void draw_background(cairo_t *cs) {
+    /* draw the light blue background */
+    cairo_rectangle(cs, -1.0, -1.0, WIDTH + 1.0, HEIGHT + 1.0);
+    cairo_set_source_rgb(cs, 0.9, 0.9, 1.0);
+    cairo_fill(cs);
+
+    /* draw the red track */
+    cairo_set_source_rgb(cs, 0.9, 0.3, 0.3);
+    cairo_move_to(cs, 10.0, HEIGHT - 50.0);
+    cairo_line_to(cs, WIDTH - 10.0, HEIGHT - 50.0);
+    cairo_stroke(cs);
+}
+
 void init_window(void) {
     assert(!window_active);
 
@@ -43,51 +68,45 @@ void init_window(void) {
 
     /* set up the display window and background pixmap */
     window = xcb_generate_id(c);
-    xcb_pixmap_t background = xcb_generate_id(c);
     xcb_params_cw_t cw_params;
-    cw_params.back_pixmap = background;
     cw_params.event_mask = XCB_EVENT_MASK_EXPOSURE;
-    (void) xcb_aux_create_window_checked(c, XCB_COPY_FROM_PARENT,
-                                         window, screen->root,
-                                         0, 0, WIDTH, HEIGHT, 0,
-                                         XCB_WINDOW_CLASS_INPUT_OUTPUT,
-                                         screen->root_visual,
-                                         XCB_CW_BACK_PIXMAP |
-                                         XCB_CW_EVENT_MASK,
-                                         &cw_params);
-    (void) xcb_create_pixmap_checked(c, XCB_COPY_FROM_PARENT,
-                                     background, window, WIDTH, HEIGHT);
+    xcb_void_cookie_t result =
+        xcb_aux_create_window_checked(c, XCB_COPY_FROM_PARENT,
+                                      window, screen->root,
+                                      0, 0, WIDTH, HEIGHT, 0,
+                                      XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                                      screen->root_visual,
+                                      XCB_CW_EVENT_MASK,
+                                      &cw_params);
+    check_request("create_window", result);
+    xcb_pixmap_t background = xcb_generate_id(c);
+    result = xcb_create_pixmap_checked(c, 24, background, window,
+                                       WIDTH, HEIGHT);
+    check_request("create_pixmap", result);
+    cw_params.back_pixmap = background;
+    result = xcb_aux_change_window_attributes_checked(c, window,
+                                                      XCB_CW_BACK_PIXMAP,
+                                                      &cw_params);
+    check_request("change_window_attributes", result);
 
     /* create and paint the background surface */
     xcb_visualtype_t *visualtype =
         xcb_aux_find_visual_by_id(screen, screen->root_visual);
     assert(visualtype);
     cairo_surface_t *surface =
-        cairo_xcb_surface_create(c, background, visualtype,
-                                 WIDTH, HEIGHT);
+        cairo_xcb_surface_create(c, background, visualtype, WIDTH, HEIGHT);
     assert(surface);
     cairo_t *cs = cairo_create(surface);
     assert(cs);
-    cairo_rectangle(cs, 0.0, 0.0, WIDTH, HEIGHT);
-    cairo_set_source_rgb(cs, 0.0, 0.0, 0.5);
-    cairo_fill(cs);
-    cairo_move_to(cs, 10.0, HEIGHT - 10.0);
-    cairo_line_to(cs, WIDTH - 10.0, HEIGHT - 10.0);
+    draw_background(cs);
     cairo_destroy(cs);
 
     /* get the window onscreen and rendered */
-    (void) xcb_map_window_checked(c, window);
+    result = xcb_clear_area_checked(c, 0, window, 0, 0, WIDTH, HEIGHT);
+    check_request("clear_area", result);
+    result = xcb_map_window_checked(c, window);
+    check_request("map_window", result);
     (void) xcb_aux_sync(c);
-
-    do {
-        fprintf(stderr, "waiting for exposure\n");
-        xcb_generic_event_t * event = xcb_wait_for_event(c);
-        if ((event->response_type & ~0x80) != XCB_EXPOSE)
-            continue;
-        fprintf(stderr, "exposure received\n");
-        (void) xcb_clear_area_checked(c, 0, window, 0, 0, WIDTH, HEIGHT);
-        (void) xcb_aux_sync(c);
-    } while(0);
 
     window_active = 1;
 }
