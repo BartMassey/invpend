8094c7e0fdb3e149c48e6f8cda6bf913f8b1dcf0
Bart Massey <bart@cs.pdx.edu>
2017-07-13 16:56:21 -0700

    cleaned up plot for readability

--- show.sh
+++ show.sh
@@ -7,7 +7,9 @@ SCRIPT=/tmp/show.$$
 trap "rm $SCRIPT" 0 1 2 3 15
 cat >$SCRIPT <<EOF
 set term wxt persist;
-plot '$1' using 1:2 with lines;
-replot '$1' using 1:3 with lines;
+set xlabel 'generation'
+set ylabel 'time (sim secs)'
+plot '$1' using 1:2 with lines title 'max';
+replot '$1' using 1:3 with lines title 'avg';
 EOF
 gnuplot $SCRIPT
